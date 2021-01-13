001efdf7722f507d48aa10246805b6a0373cd01d
Alex <test@example.com>
2021-01-13 15:28:30 +0300

    second test app

--- script.js
+++ script.js
@@ -16,10 +16,32 @@ let personalMovieDB = {
 
 //3
 for(let i = 0; i < 2; i++){
-    let lastFilm = prompt('Один из последних просмотренных фильмов?');
+    let lastFilm = prompt('Один из последних просмотренных фильмов?','');
+    while(lastFilm == null || lastFilm.length === 0 || lastFilm.length > 50){
+        lastFilm = prompt('Повторите Один из последних просмотренных фильмов?','');
+    }
     let grade = prompt('На сколько его оцениваете?');
+    while(grade == null || grade.length === 0 || grade.length > 50){
+        grade = prompt('Повторите один из последних просмотренных фильмов?','');
+    }
 
     personalMovieDB.movies[lastFilm] = grade;
 }
  
 console.log(personalMovieDB);
+
+const SMALL_COUNT = 10;
+const BIG_COUNT = 30;
+
+if(personalMovieDB.count < SMALL_COUNT){
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if(personalMovieDB.count < BIG_COUNT){
+    console.log("Вы классический зритель");
+}
+else if(personalMovieDB.count > BIG_COUNT) {
+    console.log("Вы киноман");
+}
+else{
+    console.log("Произошла ошибка");
+}
